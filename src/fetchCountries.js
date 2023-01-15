29ae8d651256ccf671e4d17c9809df976227adc9
Dawid <davidowsky9519@gmail.com>
2023-01-15 18:54:15 +0100

    trim

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export const fetchCountries = name => {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    `https://restcountries.com/v3.1/name/${name.trim()}?fields=name,capital,population,flags,languages`
   ).then(res => {
     if (!res.ok) {
       throw new Error(response.status);
